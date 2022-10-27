fa09ddfc18f52e0cb5164cee5c9b29942cbadc80
Alvaro Avila <>
2022-10-27 19:25:57 -0400

    Added the TCs for social media icons validation

---
+++ cypress/e2e/business_insider/NavigationDrawerObjects.js
@@ -0,0 +1,11 @@
+class NavigationDrawerObjects {
+    hamburgerButton = () => 'button[data-e2e-name="hamburgerBtn"]';
+    drawerComponent = () => 'nav[data-component-type="drawer"]';
+    facebookIcon = () => '[data-e2e-name="facebook"]'
+    twitterIcon = () => '[data-e2e-name="twitter"]'
+    linkedinIcon = () => '[data-e2e-name="linkedin"]'
+    youtubeIcon = () => '[data-e2e-name="youtube"]'
+    instagramIcon = () => '[data-e2e-name="instagram"]'
+}
+
+export default NavigationDrawerObjects;
---
+++ cypress/e2e/navigation.cy.js
@@ -0,0 +1,65 @@
+import NavigationDrawerObjects from './business_insider/NavigationDrawerObjects';
+
+
+const url = 'https://www.businessinsider.com/ikea-nyc-store-planning-studio-tour-2019-4'
+const facebook_insider_url = 'https://www.facebook.com/businessinsider'    
+const twitter_insider_url = 'https://twitter.com/businessinsider'
+const linkedin_insider_url = 'https://www.linkedin.com/company/businessinsider/'
+const youtube_insider_url = 'https://www.youtube.com/user/businessinsider'
+const instagram_insider_url = 'https://www.instagram.com/businessinsider/'
+const navigationDrawerObjects = new NavigationDrawerObjects()
+
+context('Navigation', () => {
+  beforeEach(() => {
+    cy.visit(url)
+  })
+
+  it('Verify Social Media Navigation to Facebook - Businessinsider', () => {
+
+    cy.get(navigationDrawerObjects.hamburgerButton()).trigger('mouseover')
+    cy.get(navigationDrawerObjects.hamburgerButton()).click()
+    cy.get(navigationDrawerObjects.drawerComponent()).scrollTo('bottom')
+    
+    cy.get(navigationDrawerObjects.facebookIcon()).invoke('attr', 'href').should('eq', facebook_insider_url)
+    cy.get(navigationDrawerObjects.facebookIcon()).invoke('attr', 'target').should('eq', "_blank")    
+  }),
+  
+  it('Verify Social Media Navigation to Twitter - Businessinsider', () => {
+
+    cy.get(navigationDrawerObjects.hamburgerButton()).trigger('mouseover')
+    cy.get(navigationDrawerObjects.hamburgerButton()).click()
+    cy.get(navigationDrawerObjects.drawerComponent()).scrollTo('bottom')
+
+    cy.get(navigationDrawerObjects.twitterIcon()).invoke('attr', 'href').should('eq', twitter_insider_url)
+    cy.get(navigationDrawerObjects.twitterIcon()).invoke('attr', 'target').should('eq', "_blank")
+  }),
+  
+  it('Verify Social Media Navigation to Linkedin - Businessinsider', () => {
+    cy.get(navigationDrawerObjects.hamburgerButton()).trigger('mouseover')
+    cy.get(navigationDrawerObjects.hamburgerButton()).click()
+    cy.get(navigationDrawerObjects.drawerComponent()).scrollTo('bottom')
+        
+    cy.get(navigationDrawerObjects.linkedinIcon()).invoke('attr', 'href').should('eq', linkedin_insider_url)
+    cy.get(navigationDrawerObjects.linkedinIcon()).invoke('attr', 'target').should('eq', "_blank")
+  }),
+  
+  it('Verify Social Media Navigation to Youtube - Businessinsider', () => {
+
+    cy.get(navigationDrawerObjects.hamburgerButton()).trigger('mouseover')
+    cy.get(navigationDrawerObjects.hamburgerButton()).click()
+    cy.get(navigationDrawerObjects.drawerComponent()).scrollTo('bottom')
+    
+    cy.get(navigationDrawerObjects.youtubeIcon()).invoke('attr', 'href').should('eq', youtube_insider_url)
+    cy.get(navigationDrawerObjects.youtubeIcon()).invoke('attr', 'target').should('eq', "_blank")
+  }),
+
+  it('Verify Social Media Navigation to Instagram - Businessinsider', () => {
+
+    cy.get(navigationDrawerObjects.hamburgerButton()).trigger('mouseover')
+    cy.get(navigationDrawerObjects.hamburgerButton()).click()
+    cy.get(navigationDrawerObjects.drawerComponent()).scrollTo('bottom')
+    
+    cy.get(navigationDrawerObjects.instagramIcon()).invoke('attr', 'href').should('eq', instagram_insider_url)
+    cy.get(navigationDrawerObjects.instagramIcon()).invoke('attr', 'target').should('eq', "_blank")
+  })
+})
